591827cc09fe159fb7edaa0e38180d4f1c90b707
vivek9142 <vm199841@gmail.com>
2022-03-14 15:46:28 +0530

    added the fundamental part guiding parts

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -30,7 +30,7 @@ function App() {
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
@@ -42,3 +42,15 @@ function App() {
 }
 
 export default App
+
+//If we add,change,remove some elements of array,so react doesnt hav any idea what causes
+//the particular change in JSX you're returning all it knows the JSX is changed so it does its best to 
+//update the dom based on changes you've provided but the default value or the value of elements will not be kept
+//up to date by react even if we provide the new default value for this.so sometimes it guesses right but it might be
+//wrong sometimes. 
+//So we add a key prop so react will know if we have remove change or add any item in array since it track the element over time
+
+//provide a unique key,but dont provide the random generating a key since it needs to be consistent for the react element that is 
+//representing the particular item. so having the id is preferable since it is unique for each of these items.but if you dont have it then
+//maybe generate the id for items before rendering them or you could use something else about the item that is unique. but don't 
+//try to use the index of item for key,since it will display the default behaviour i.e, inconsistent as before.
